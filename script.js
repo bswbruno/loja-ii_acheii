a3e26c35566d04b8073dc62ed2dd6ec2f2b799b4
Bruno Santos <brunusuxx@gmail.com>
2024-11-01 02:28:24 -0300

    att

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const products = [
     { id: 1, name: 'Prateleira 4 andares', img: 'img/organizador_4_andares.png', link: 'https://s.shopee.com.br/BCEwhUqPb', code: '0001' },
     { id: 2, name: 'Parafusadeira/Furadeira', img: 'img/parafusadeira1.png', link: 'https://s.shopee.com.br/6KmsL8Qfms', code: '0002' },
     { id: 3, name: 'Piscina Insuflável', img: 'img/Piscina_Insuflável.png', link: 'https://s.shopee.com.br/5AauxEpNuk', code: '0003' },
-    { id: 4, name: 'Kit jogo de Cozinha', img: 'img/talheres_de_siliconne.png', link: 'https://s.shopee.com.br/9zgBDmaFO5', code: '0003' },
+    { id: 4, name: 'Kit jogo de Cozinha', img: 'img/talheres_de_siliconne.png', link: 'https://s.shopee.com.br/9zgBDmaFO5', code: '0004' },
     
     // ... adicione mais produtos conforme necessário
 ];
